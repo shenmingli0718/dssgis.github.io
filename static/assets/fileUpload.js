bac6acdf2dbe11f6c26359da0ed74b8d3ba354e9
shenmingli0718 <shenmingli@gmail.com>
2025-12-31 15:23:36 +0800

    update fileUpload.js & download.html

--- static/assets/fileUpload.js
+++ static/assets/fileUpload.js
@@ -65,7 +65,8 @@ async function uploadFile(file,subid) {
         formData.append('subid', subid);
 
         try {
-            const response = await fetch('http://localhost:3000/2025_aut_Python_proj', {
+            // const response = await fetch('http://localhost:3000/2025_aut_Python_proj', {
+            const response = await fetch('https://dssgisapi-github-io.onrender.com/2025_aut_Python_proj', {    
             // const response = await fetch('https://ntgisapigithubio-production.up.railway.app/2024_aut_Python_proj', {
             // const response = await fetch('https://ntgisapi.zeabur.app/2024_aut_Python_proj', {
                 method: 'POST',
@@ -95,7 +96,8 @@ async function check_capacitylimit(uploadChunks) {
     formData.append('uploadChunks', uploadChunks);
 
     try {
-         const response = await fetch('http://localhost:3000/check_capacity_limit', {
+        //  const response = await fetch('http://localhost:3000/check_capacity_limit', {
+        const response = await fetch('https://dssgisapi-github-io.onrender.com/check_capacity_limit', {
         // 
         // const response = await fetch('https://ntgisapigithubio-production.up.railway.app/check_capacity_limit', {
         // const response = await fetch('https://ntgisapi.zeabur.app/check_capacity_limit', {
